bba8056c5e27a095ae64c7a221534fb0a0ea94b0
Jean-Baptiste Aubry <jbaubry25@gmail.com>
2021-03-02 23:02:57 +0100

    m4

--- src/Form/SequencetheoriqueType.php
+++ src/Form/SequencetheoriqueType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Categoriesequence;
 use App\Entity\Sequencetheorique;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,21 @@ class SequencetheoriqueType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('niveau')
-            ->add('idcategoriesequence')
+            ->add('titre', TextType::class, ['label' => 'Titre'])
+            ->add('niveau', ChoiceType::class, [
+                'choices'  => [
+                    'Débutant' => 1,
+                    'Confirmé' => 2,
+                    'Expert' => 3,
+                ],
+            ])
+            ->add('idcategoriesequence', EntityType::class, [
+                'class' => Categoriesequence::class,
+                'choice_label' => function ($category) {
+                    return $category->getTitre();
+                },
+                'label' => 'Catégorie',
+            ])
         ;
     }
 
